36411b52c9444dde07b823d5c809c26317a1a2eb
Shivangi Shukla <shivangishukla1911@gmail.com>
2025-06-07 22:17:02 +0530

    maximumConsecutive numbers

---
+++ maxconsecutive.js
@@ -0,0 +1,19 @@
+const prices = [1, 1, 0, 1, 1, 1];
+console.log(maxConsecutive(prices));
+function maxConsecutive(nums) {
+  let maxCount = 0;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      count++;
+      if (count > maxCount) {
+        maxCount = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+  return maxCount;
+}
+
+// Output: 3
