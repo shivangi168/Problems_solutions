405f1fb4f05328e11be9187ecbdc1f4f1b906351
Shivangi Shukla <shivangishukla1911@gmail.com>
2025-06-07 18:11:27 +0530

    Create formatlist.js

---
+++ formatlist.js
@@ -0,0 +1,12 @@
+function formatList(items) {
+  const len = items.length;
+  if (len === 0) return "";
+  if (len === 1) return items[0];
+  if (len === 2) return `${items[0]} and ${items[1]}`;
+  return `${items.slice(0, len - 1).join(", ")} and ${items[le - 1]}`;
+}
+
+//For the purpose of user debugging.
+formatList(["apple", "banana"]);
+
+module.exports = formatList;
