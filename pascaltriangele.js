e2628997a97673b08833f5bd397898b1463b938f
Shivangi Shukla <kbajpai@datatech.com.mt>
2025-06-17 22:16:14 +0530

    Create pascaltriangele.js

---
+++ pascaltriangele.js
@@ -0,0 +1,24 @@
+function generatePascalsTriangle(n) {
+  let triangle = [];
+
+  for (let row = 0; row < n; row++) {
+    let currentRow = [];
+
+    for (let col = 0; col <= row; col++) {
+      
+      if (col === 0 || col === row) {
+        currentRow.push(1);
+      } else {
+       
+        const val = triangle[row - 1][col - 1] + triangle[row - 1][col];
+        currentRow.push(val);
+      }
+    }
+
+    triangle.push(currentRow);
+  }
+
+  return triangle;
+}
+const result = generatePascalsTriangle(5);
+console.log(result);
