9f00541e8493885eb13f3848d0520bed56f9ab0f
Shivangi Shukla <shivangishukla1911@gmail.com>
2025-06-25 20:42:29 +0530

    Create searchInratotedArray.js

---
+++ searchInratotedArray.js
@@ -0,0 +1,32 @@
+function search(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        // Found the target
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        // Left half is sorted
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1; // Target not found
+}
