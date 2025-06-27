db702b174b2eb8804b2d9d46d5eb0274b7f55a93
Shivangi Shukla <shivangishukla1911@gmail.com>
2025-06-27 16:26:09 +0530

    Update findMinInroatedArray.js

--- findMinInroatedArray.js
+++ findMinInroatedArray.js
@@ -11,5 +11,7 @@ function findMin(nums) {
         }
     }
 
-    return nums[end]; // or nums[start], both are same here
+    return nums[end]; 
 }
+const nums = [4, 5, 6, 7, 0, 1, 2];
+console.log(findMin(nums)); // Output: 0
