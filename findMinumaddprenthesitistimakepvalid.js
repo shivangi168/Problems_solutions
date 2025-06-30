0fc75c4880fc9aa784c3ed71e015f04beba1f408
Shivangi Shukla <shivangishukla1911@gmail.com>
2025-06-30 11:49:30 +0530

    Create findMinumaddprenthesitistimakepvalid.js

---
+++ findMinumaddprenthesitistimakepvalid.js
@@ -0,0 +1,21 @@
+const str = "(((";
+const str2 = "())";
+console.log(findMinimum(str2));
+console.log(findMinimum(str));
+
+function findMinimum(str){
+    let count = 0;
+    const res = [];
+   let openingFre = 0;
+    let closingfreq = 0;
+    for(let ch of str){
+        if(ch == '('){
+            openingFre++;
+        }
+        else{
+            closingfreq++;
+        }
+    }
+    return Math.abs(openingFre);
+    
+}
