9f101c9917c43e46aa16d41c2b19673ad12bb51d
Shivangi Shukla <shivangishukla1911@gmail.com>
2025-07-01 11:12:41 +0530

    Update PrintDecreasingRecursion.js

--- PrintDecreasingRecursion.js
+++ PrintDecreasingRecursion.js
@@ -1,7 +1,20 @@
-printDecreasing(5);
-function printDecreasing(n){
+printDecreasingIncreasing(5);
+function printDecreasingIncreasing(n){
     if(n==0) return;
+    console.log(n);
     printDecreasing(n-1);
     console.log(n);
+
     
 }
+
+function findFactorial(n){
+    if(n==1) return 1;
+    
+    let fnm1 = findFactorial(n-1);
+    let fact = n*fnm1;
+    return fact;
+    // console.log(fact);
+    
+}
+console.log(findFactorial(5));
