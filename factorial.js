19e48f13754dc44fb47864d2f708fda7dbac5258
Shivangi Shukla <64777788+shivangi168@users.noreply.github.com>
2025-11-02 17:40:03 +0530

    factorial

--- factorial.js
+++ factorial.js
@@ -1,11 +1,20 @@
-function factorial(n) {
-    if (n < 0) return -1; // factorial not defined for negative numbers
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-      result *= i;
+console.log(factorial(5));
+console.log(factorial(3));
+
+function factorial(n){
+    let fact = 1;
+    if(n<0){
+        return "Invalid";
     }
-    return result;
-  }
-  
-  console.log(factorial(5)); // Output: 120
-  
+    for(let i=1;i<=n;i++){
+        fact = fact*i;
+    }
+    return fact;
+}
+function factorial(n) {
+  if (n < 0) return "Factorial not defined for negative numbers";
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // Output: 120
